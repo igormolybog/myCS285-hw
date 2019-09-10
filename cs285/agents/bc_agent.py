3a51edce55e5cbc3dd08257a42cdc1b652f7a716
gary <igormolybog@gmail.com>
2019-09-09 18:26:48 -0700

    implemented bc_agent

--- cs285/agents/bc_agent.py
+++ cs285/agents/bc_agent.py
@@ -24,7 +24,7 @@ class BCAgent(BaseAgent):
                                self.agent_params['size'],
                                discrete = self.agent_params['discrete'],
                                learning_rate = self.agent_params['learning_rate'],
-                               ) ## TODO: look in here and implement this
+                               ) ## DONE: look in here and implement this
 
         # replay buffer
         self.replay_buffer = ReplayBuffer(self.agent_params['max_replay_buffer_size'])
@@ -32,10 +32,10 @@ class BCAgent(BaseAgent):
     def train(self, ob_no, ac_na, re_n, next_ob_no, terminal_n):
         # training a BC agent refers to updating its actor using
         # the given observations and corresponding action labels
-        self.actor.update(ob_no, ac_na) ## TODO: look in here and implement this
+        self.actor.update(ob_no, ac_na) ## DONE: look in here and implement this
 
     def add_to_replay_buffer(self, paths):
         self.replay_buffer.add_rollouts(paths)
 
     def sample(self, batch_size):
-        return self.replay_buffer.sample_random_data(batch_size) ## TODO: look in here and implement this
+        return self.replay_buffer.sample_random_data(batch_size) ## DONE: look in here and implement this
