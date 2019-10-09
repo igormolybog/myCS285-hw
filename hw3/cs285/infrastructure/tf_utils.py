32244441926ce2015a0778447fec73f44701417c
gary <igormolybog@gmail.com>
2019-10-08 20:27:29 -0700

    implemented actor-critic

--- hw3/cs285/infrastructure/tf_utils.py
+++ hw3/cs285/infrastructure/tf_utils.py
@@ -5,9 +5,31 @@ import os
 
 
 def build_mlp(input_placeholder, output_size, scope, n_layers, size, activation=tf.tanh, output_activation=None):
-    
-    # TODO: GETTHIS from HW1
 
+    # DONE: GETTHIS from HW1
+    """
+        Builds a feedforward neural network
+
+        arguments:
+            input_placeholder: placeholder variable for the state (batch_size, input_size)
+            scope: variable scope of the network
+
+            n_layers: number of hidden layers
+            size: dimension of each hidden layer
+            activation: activation of each hidden layer
+
+            output_size: size of the output layer
+            output_activation: activation of the output layer
+
+        returns:
+            output_placeholder: the result of a forward pass through the hidden layers + the output layer
+    """
+    output_placeholder = input_placeholder
+    with tf.variable_scope(scope):
+        for _ in range(n_layers):
+            output_placeholder = tf.layers.dense(inputs=output_placeholder, units=size, activation=activation) # DONE HINT: use tf.layers.dense (specify <input>, <size>, activation=<?>)
+        output_placeholder = tf.layers.dense(inputs=output_placeholder, units=output_size, activation=output_activation) # DONE HINT: use tf.layers.dense (specify <input>, <size>, activation=<?>)
+    return output_placeholder
 
 ############################################
 ############################################
