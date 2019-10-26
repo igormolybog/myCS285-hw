8d682126a8476e506b218919ac1d2282ca42053b
gary <igormolybog@gmail.com>
2019-10-26 12:45:03 -0700

    implemented policy and model

--- hw4/cs285/policies/MPC_policy.py
+++ hw4/cs285/policies/MPC_policy.py
@@ -4,7 +4,7 @@ from .base_policy import BasePolicy
 
 
 class MPCPolicy(BasePolicy):
-
+# MODEL PREDICTIVE CONTROL
     def __init__(self,
         sess,
         env,
@@ -20,7 +20,7 @@ class MPCPolicy(BasePolicy):
         self.env = env
         self.dyn_models = dyn_models
         self.horizon = horizon
-        self.N = N
+        self.N = N # Number of sequences to generate to optimize over
         self.data_statistics = None # NOTE must be updated from elsewhere
 
         self.ob_dim = self.env.observation_space.shape[0]
@@ -32,9 +32,11 @@ class MPCPolicy(BasePolicy):
         self.high = self.ac_space.high
 
     def sample_action_sequences(self, num_sequences, horizon):
-        # TODO(Q1) uniformly sample trajectories and return an array of
+        # DINE(Q1) uniformly sample trajectories and return an array of
         # dimensions (num_sequences, horizon, self.ac_dim)
-        return random_action_sequences
+        samp = np.array([self.ac_space.sample() for _ in range(num_sequences*horizon)])
+        samp = samp.reshape((num_sequences,horizon) + samp.shape[1:])
+        return samp
 
     def get_action(self, obs):
 
@@ -46,24 +48,28 @@ class MPCPolicy(BasePolicy):
         candidate_action_sequences = self.sample_action_sequences(num_sequences=self.N, horizon=self.horizon)
 
         # a list you can use for storing the predicted reward for each candidate sequence
-        predicted_rewards_per_ens = []
+        predicted_rewards_per_ens = [[None]*len(candidate_action_sequences)]*len(self.dyn_models)
 
-        for model in self.dyn_models:
-            pass
-            # TODO(Q2)
+        for i, model in enumerate(self.dyn_models):
+            # DONE(Q2)
 
             # for each candidate action sequence, predict a sequence of
             # states for each dynamics model in your ensemble
+            for j, action_sequence in enumerate(candidate_action_sequences)
+                states_sequence = model.run_plan(obs, action_sequence, data_statistics)
 
             # once you have a sequence of predicted states from each model in your
             # ensemble, calculate the reward for each sequence using self.env.get_reward (See files in envs to see how to call this)
+                rewards, dones = self.env.get_reward([obs]+states_sequence[:-1], action_sequence)
+                predicted_rewards_per_ens[i][j] = sum(rewards)
 
         # calculate mean_across_ensembles(predicted rewards).
         # the matrix dimensions should change as follows: [ens,N] --> N
-        predicted_rewards = None # TODO(Q2)
+        predicted_rewards = np.array(predicted_rewards_per_ens).mean(axis=0) # DONE(Q2)
+        assert len(predicted_rewards) == self.N
 
         # pick the action sequence and return the 1st element of that sequence
-        best_index = None #TODO(Q2)
-        best_action_sequence = None #TODO(Q2)
-        action_to_take = None # TODO(Q2)
+        best_index = np.argmax(predicted_rewards) #DONE(Q2)
+        best_action_sequence = candidate_action_sequences[best_index] #DONE(Q2)
+        action_to_take = best_action_sequence[0] # DONE(Q2)
         return action_to_take[None] # the None is for matching expected dimensions
