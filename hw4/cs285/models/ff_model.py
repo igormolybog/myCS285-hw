530677b9b984ec837da7042f0d1bf4694236b9b1
gary <igormolybog@gmail.com>
2019-10-26 19:23:44 -0700

    git chache refilled

---
+++ hw4/cs285/models/ff_model.py
@@ -0,0 +1,119 @@
+import tensorflow as tf
+
+from .base_model import BaseModel
+from cs285.infrastructure.utils import normalize, unnormalize
+from cs285.infrastructure.tf_utils import build_mlp
+
+
+class FFModel(BaseModel):
+
+    def __init__(self, sess, ac_dim, ob_dim, n_layers, size, learning_rate=0.001, scope='dyn_model'):
+        super(FFModel, self).__init__()
+
+        # init vars
+        # self.env = env
+        self.sess = sess
+        self.ac_dim = ac_dim
+        self.ob_dim = ob_dim
+        self.n_layers = n_layers
+        self.size = size
+        self.learning_rate = learning_rate
+        self.scope = scope
+
+        # build TF graph
+        with tf.variable_scope(self.scope, reuse=tf.AUTO_REUSE):
+            self.build_graph()
+        self.define_train_op()
+
+    #############################
+
+    def build_graph(self):
+        self.define_placeholders()
+        self.define_forward_pass()
+
+    def define_placeholders(self):
+
+        self.obs_pl = tf.placeholder(shape=[None, self.ob_dim], name="ob", dtype=tf.float32)
+        self.acs_pl = tf.placeholder(shape=[None, self.ac_dim], name="ac", dtype=tf.float32)
+        self.delta_labels = tf.placeholder(shape=[None, self.ob_dim], name="labels", dtype=tf.float32)
+
+        self.obs_mean_pl = tf.placeholder(shape=[self.ob_dim], name="obs_mean", dtype=tf.float32)
+        self.obs_std_pl = tf.placeholder(shape=[self.ob_dim], name="obs_std", dtype=tf.float32)
+        self.acs_mean_pl = tf.placeholder(shape=[self.ac_dim], name="acs_mean", dtype=tf.float32)
+        self.acs_std_pl = tf.placeholder(shape=[self.ac_dim], name="acs_std", dtype=tf.float32)
+        self.delta_mean_pl = tf.placeholder(shape=[self.ob_dim], name="delta_mean", dtype=tf.float32)
+        self.delta_std_pl = tf.placeholder(shape=[self.ob_dim], name="delta_std", dtype=tf.float32)
+
+    def define_forward_pass(self):
+        # normalize input data to mean 0, std 1
+        obs_unnormalized = self.obs_pl
+        acs_unnormalized = self.acs_pl
+        # Hint: Consider using the normalize function defined in infrastructure.utils for the following two lines
+        obs_normalized = normalize(obs_unnormalized, self.obs_mean_pl, self.obs_std_pl) # DONE(Q1) Define obs_normalized using obs_unnormalized,and self.obs_mean_pl and self.obs_std_pl
+        acs_normalized = normalize(acs_unnormalized, self.acs_mean_pl, self.acs_std_pl) # DONE(Q2) Define acs_normalized using acs_unnormalized and self.acs_mean_pl and self.acs_std_pl
+
+        # predicted change in obs
+        concatenated_input = tf.concat([obs_normalized, acs_normalized], axis=1)
+        # Hint: Note that the prefix delta is used in the variable below to denote changes in state, i.e. (s'-s)
+        self.delta_pred_normalized = build_mlp(input_placeholder=concatenated_input,
+                                                output_size=self.ob_dim,
+                                                scope=self.scope, n_layers=self.n_layers,
+                                                size=self.size) # DONE(Q1) Use the build_mlp function and the concatenated_input above to define a neural network that predicts unnormalized delta states (i.e. change in state)
+        self.delta_pred_unnormalized = unnormalize(self.delta_pred_normalized, self.delta_mean_pl, self.delta_std_pl) # DONE(Q1) Unnormalize the the delta_pred above using the unnormalize function, and self.delta_mean_pl and self.delta_std_pl
+        self.next_obs_pred = obs_unnormalized + self.delta_pred_unnormalized # DONE(Q1) Predict next observation using current observation and delta prediction (not that next_obs here is unnormalized)
+
+    def define_train_op(self):
+
+        # normalize the labels
+        self.delta_labels_normalized = normalize(self.delta_labels, self.delta_mean_pl, self.delta_std_pl) # DONE(Q1) Define a normalized version of delta_labels using self.delta_labels (which are unnormalized), and self.delta_mean_pl and self.delta_std_pl
+
+        # compared predicted deltas to labels (both should be normalized)
+        self.loss = tf.losses.mean_squared_error(labels=self.delta_labels_normalized,
+                                                predictions=self.delta_pred_normalized) # DONE(Q1) Define a loss function that takes as input normalized versions of predicted change in state and ground truth change in state
+        self.train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss) # DONE(Q1) Define a train_op to minimize the loss defined above. Adam optimizer will work well.
+
+    #############################
+
+    def get_prediction(self, obs, acs, data_statistics):
+        if len(obs.shape) > 1:
+            observations = obs
+        else:
+            observations = obs[None]
+
+        if len(acs.shape) > 1:
+            actions = acs
+        else:
+            actions = acs[None]
+
+        feed_dict = {self.obs_pl: observations,
+                    self.acs_pl: actions,
+                    self.obs_mean_pl: data_statistics['obs_mean'],
+                    self.obs_std_pl: data_statistics['obs_std'],
+                    self.acs_mean_pl: data_statistics['acs_mean'],
+                    self.acs_std_pl: data_statistics['acs_std'],
+                    self.delta_mean_pl: data_statistics['delta_mean'],
+                    self.delta_std_pl: data_statistics['delta_std']}
+        return self.sess.run(self.next_obs_pred, feed_dict=feed_dict) # DONE(Q1) Run model prediction on the given batch of data
+
+    def update(self, observations, actions, next_observations, data_statistics):
+        # train the model
+        feed_dict = {self.delta_labels: next_observations - observations,
+                    self.obs_pl: observations,
+                    self.acs_pl: actions,
+                    self.obs_mean_pl: data_statistics['obs_mean'],
+                    self.obs_std_pl: data_statistics['obs_std'],
+                    self.acs_mean_pl: data_statistics['acs_mean'],
+                    self.acs_std_pl: data_statistics['acs_std'],
+                    self.delta_mean_pl: data_statistics['delta_mean'],
+                    self.delta_std_pl: data_statistics['delta_std']}
+        _, loss = self.sess.run([self.train_op, self.loss], feed_dict=feed_dict) # DONE(Q1) Run the defined train_op here, and also return the loss being optimized (on this batch of data)
+        return loss
+
+    # SELF-IMPLEMENTED
+    def run_plan(self, starting_observation, planned_actions_sequence, data_statistics):
+        observation = starting_observation
+        planned_obs = [None]*len(planned_actions_sequence)
+        for i, action in enumerate(planned_actions_sequence):
+            observation = self.get_prediction(observation, action, data_statistics)
+            planned_obs[i] = list(observation[0])
+        return planned_obs
---
+++ hw4/cs285/policies/MPC_policy.py
@@ -0,0 +1,76 @@
+import numpy as np
+
+from .base_policy import BasePolicy
+
+
+class MPCPolicy(BasePolicy):
+# MODEL PREDICTIVE CONTROL
+    def __init__(self,
+        sess,
+        env,
+        ac_dim,
+        dyn_models,
+        horizon,
+        N,
+        **kwargs):
+        super().__init__(**kwargs)
+
+        # init vars
+        self.sess = sess
+        self.env = env
+        self.dyn_models = dyn_models
+        self.horizon = horizon
+        self.N = N # Number of sequences to generate to optimize over
+        self.data_statistics = None # NOTE must be updated from elsewhere
+
+        self.ob_dim = self.env.observation_space.shape[0]
+
+        # action space
+        self.ac_space = self.env.action_space
+        self.ac_dim = ac_dim
+        self.low = self.ac_space.low
+        self.high = self.ac_space.high
+
+    def sample_action_sequences(self, num_sequences, horizon):
+        # DINE(Q1) uniformly sample trajectories and return an array of
+        # dimensions (num_sequences, horizon, self.ac_dim)
+        samp = np.array([self.ac_space.sample() for _ in range(num_sequences*horizon)])
+        samp = samp.reshape((num_sequences,horizon) + samp.shape[1:])
+        return samp
+
+    def get_action(self, obs):
+
+        if self.data_statistics is None:
+            # print("WARNING: performing random actions.")
+            return self.sample_action_sequences(num_sequences=1, horizon=1)[0]
+
+        #sample random actions (Nxhorizon)
+        candidate_action_sequences = self.sample_action_sequences(num_sequences=self.N, horizon=self.horizon)
+
+        # a list you can use for storing the predicted reward for each candidate sequence
+        predicted_rewards_per_ens = [[None]*len(candidate_action_sequences)]*len(self.dyn_models)
+
+        for i, model in enumerate(self.dyn_models):
+            # DONE(Q2)
+
+            # for each candidate action sequence, predict a sequence of
+            # states for each dynamics model in your ensemble
+            for j, action_sequence in enumerate(candidate_action_sequences):
+                states_sequence = model.run_plan(obs, action_sequence, self.data_statistics)
+
+            # once you have a sequence of predicted states from each model in your
+            # ensemble, calculate the reward for each sequence using self.env.get_reward (See files in envs to see how to call this)
+                rewarded_states = np.vstack([np.expand_dims(obs, axis=0), np.array(states_sequence[:-1])])
+                rewards, dones = self.env.get_reward(rewarded_states, action_sequence)
+                predicted_rewards_per_ens[i][j] = sum(rewards)
+
+        # calculate mean_across_ensembles(predicted rewards).
+        # the matrix dimensions should change as follows: [ens,N] --> N
+        predicted_rewards = np.array(predicted_rewards_per_ens).mean(axis=0) # DONE(Q2)
+        assert len(predicted_rewards) == self.N
+
+        # pick the action sequence and return the 1st element of that sequence
+        best_index = np.argmax(predicted_rewards) #DONE(Q2)
+        best_action_sequence = candidate_action_sequences[best_index] #DONE(Q2)
+        action_to_take = best_action_sequence[0] # DONE(Q2)
+        return action_to_take[None] # the None is for matching expected dimensions
